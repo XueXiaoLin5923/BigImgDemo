afb647c96baef43eefaea51c848837052b802ead
薛晓林 <xiao592369639@126.com>
2017-01-11 13:57:08 +0800

    Initial Commit

---
+++ BigImgDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BigImgDemo
+//
+//  Created by 薛晓林 on 2017/1/11.
+//  Copyright © 2017年 薛晓林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BigImgDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BigImgDemo
+//
+//  Created by 薛晓林 on 2017/1/11.
+//  Copyright © 2017年 薛晓林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
