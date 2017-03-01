fbda9f198c87a86e6f5517525dff88edfa63ffac
XueXiaoLin <xiao592369639@126.com>
2017-03-01 11:04:42 +0800

    MyBigImgView
    MyBigImgView

--- BigImgDemo/ViewController.m
+++ BigImgDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
-
+#import "UIImageView+WebCache.h"
+#import "CMPhotoBrowser.h"
+#import "CMPhoto.h"
 @interface ViewController ()
+
+@property(nonatomic , strong)UIImageView *headerImg;
 
 @end
 
@@ -17,12 +21,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self createUI];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)createUI{
+    self.headerImg = [[UIImageView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    self.headerImg.userInteractionEnabled = YES;
+    [self.headerImg sd_setImageWithURL:[NSURL URLWithString:@"http://139.196.208.240:801/images/upload/avatars/20151003163407/medium.jpg"]];
+    [self.view addSubview:self.headerImg];
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapClick)];
+    [self.headerImg addGestureRecognizer:tap];
+}
+
+- (void)tapClick{
+    
+    NSMutableArray *browseItemArray = [[NSMutableArray alloc]init];
+    CMPhoto *browseItem = [[CMPhoto alloc]init];
+    browseItem.srcImageUrl = @"http://139.196.208.240:801/images/upload/avatars/20151003163407/large.jpg";// 大图url地址
+    browseItem.placeholder = self.headerImg;// 小图
+    [browseItemArray addObject:browseItem];
+    
+    CMPhotoBrowser *bvc = [[CMPhotoBrowser alloc]init];
+    bvc.currentPhotoIndex = 0;
+    bvc.photos = browseItemArray;
+    [bvc show];
+    
 }
 
 
